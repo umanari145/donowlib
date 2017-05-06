f20436bc7b688660f82acb33d1519062ade69341
Matsu <matsumoto@donow.jp>
2017-05-06 05:32:33 +0000

    testメソッドが誤っていたので修正

--- tests/libutil/Tests/ArrayUtilTest.php
+++ tests/libutil/Tests/ArrayUtilTest.php
@@ -3,7 +3,7 @@ namespace libutil\Tests;
 
 use libutil\ArrayUtil;
 
-class ArrayUtilTest
+class ArrayUtilTest extends \PHPUnit_Framework_TestCase
 {
 
 
@@ -12,8 +12,10 @@ class ArrayUtilTest
         $arr = [
             ['a','b','c','d'],
             ['e','f', 0,'0'],
-            ['f', 'g','h']
+            ['f','g','h']
         ];
+
+
 
         $arr2 = ArrayUtil::convertRowAndColumn($arr);
         var_dump($arr2);
